8015fd75e0e9c66e3b07e8a481d6ce1cbe3412e7
kerem212012 <rockstarfoxykerem@gmail.com>
2025-03-15 17:13:40 +0300

    add new method queryset in post

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,12 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 
 
+class PostQuerySet(models.QuerySet):
+
+    def year(self, year):
+        return self.filter(published_at__year=year).order_by('published_at')
+
+
 class Post(models.Model):
     title = models.CharField('Заголовок', max_length=200)
     text = models.TextField('Текст')
@@ -24,6 +30,7 @@ class Post(models.Model):
         'Tag',
         related_name='posts',
         verbose_name='Теги')
+    objects = PostQuerySet.as_manager()
 
     def __str__(self):
         return self.title
